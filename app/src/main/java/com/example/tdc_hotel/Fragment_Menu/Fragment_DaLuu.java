c250eb3258a7da156fb69a1469e76064bd7d13d0
sangclousi <nguvanup2003@gmail.com>
2023-11-20 19:29:05 +0700

    thông tin cá nhân khách hàng

--- app/src/main/java/com/example/tdc_hotel/Fragment_Menu/Fragment_DaLuu.java
+++ app/src/main/java/com/example/tdc_hotel/Fragment_Menu/Fragment_DaLuu.java
@@ -18,64 +18,14 @@ import com.example.tdc_hotel.R;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_DaLuu#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_DaLuu extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     RecyclerView rcvPhong;
     Spinner spTimkiem;
     ArrayList<String> arrForspTimkiem=new ArrayList<>();
-    public Fragment_DaLuu() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_DaLuu.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_DaLuu newInstance(String param1, String param2) {
-        Fragment_DaLuu fragment = new Fragment_DaLuu();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment__da_luu, container, false);
-        setControl(view);
-        Initialization();
-        return view;
-    }
     private void Initialization() {
         KQ_TimKiem_Adapter kq_timKiem_adapter=new KQ_TimKiem_Adapter();
         DividerItemDecoration dividerItemDecoration=new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL);
---
+++ app/src/main/java/com/example/tdc_hotel/Fragment_Menu/ThongTinKhach/adapter_thongtincanhan.java
@@ -0,0 +1,4 @@
+package com.example.tdc_hotel.Fragment_Menu.ThongTinKhach;
+
+public class adapter_thongtincanhan {
+}
