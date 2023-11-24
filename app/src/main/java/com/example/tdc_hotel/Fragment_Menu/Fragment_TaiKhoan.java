d8a01ed0e674e32100355ddd432462154565030b
sangclousi <nguvanup2003@gmail.com>
2023-11-24 18:57:52 +0700

    đã lưu v3

--- app/src/main/java/com/example/tdc_hotel/Fragment_Menu/Fragment_TaiKhoan.java
+++ app/src/main/java/com/example/tdc_hotel/Fragment_Menu/Fragment_TaiKhoan.java
@@ -1,13 +1,14 @@
+// Fragment_TaiKhoan.java
 package com.example.tdc_hotel.Fragment_Menu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
 import com.example.tdc_hotel.R;
 
 public class Fragment_TaiKhoan extends Fragment {
@@ -15,7 +16,47 @@ public class Fragment_TaiKhoan extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__tai_khoan, container, false);
+        View view = inflater.inflate(R.layout.fragment__tai_khoan, container, false);
+
+        // Xử lý đăng xuất khi nút được nhấn
+        Button btnLogout = view.findViewById(R.id.btnDangXuat);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLogoutDialog();
+            }
+        });
+
+        return view;
+    }
+
+    private void showLogoutDialog() {
+        // Tạo AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Xác nhận đăng xuất");
+        builder.setMessage("Bạn có chắc chắn muốn đăng xuất?");
+        builder.setPositiveButton("Đăng xuất", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Thực hiện đăng xuất ở đây
+                logout();
+            }
+        });
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Đóng dialog nếu người dùng chọn Hủy
+                dialog.dismiss();
+            }
+        });
+
+        // Hiển thị AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void logout() {
+        // Thực hiện các thao tác đăng xuất ở đây
+        // Ví dụ: chuyển đến màn hình đăng nhập
     }
 }
